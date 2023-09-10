07c3bc34a2e69e51ad7be96247bf61b7ff22c483
drteeth <drteeth339@gmail.com>
2023-09-10 10:43:30 -0400

    updated auth and server files

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,35 +1,34 @@
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
+const { AuthenticationError } = require("apollo-server-express"); // Import Apollo Server's AuthenticationError
 
-// set token secret and expiration date
-const secret = 'mysecretsshhhhh';
-const expiration = '2h';
+// Set token secret and expiration date
+const secret = "mysecretsshhhhh";
+const expiration = "2h";
 
 module.exports = {
-  // function for our authenticated routes
-  authMiddleware: function (req, res, next) {
-    // allows token to be sent via  req.query or headers
-    let token = req.query.token || req.headers.authorization;
+  authMiddleware: function ({ req }) {
+    let token = req.headers.authorization || "";
 
-    // ["Bearer", "<tokenvalue>"]
-    if (req.headers.authorization) {
-      token = token.split(' ').pop().trim();
+    if (token) {
+      // Check if the token starts with 'Bearer ' and remove it if present
+      if (token.startsWith("Bearer ")) {
+        token = token.slice(7, token.length);
+      }
+
+      if (!token) {
+        throw new AuthenticationError("You have no token!");
+      }
+
+      try {
+        // Verify token and decode user data
+        const { data } = jwt.verify(token, secret, { expiresIn: expiration });
+        return { user: data };
+      } catch (error) {
+        throw new AuthenticationError("Invalid token");
+      }
+    } else {
+      throw new AuthenticationError("Authentication token must be provided");
     }
-
-    if (!token) {
-      return res.status(400).json({ message: 'You have no token!' });
-    }
-
-    // verify token and get user data out of it
-    try {
-      const { data } = jwt.verify(token, secret, { maxAge: expiration });
-      req.user = data;
-    } catch {
-      console.log('Invalid token');
-      return res.status(400).json({ message: 'invalid token!' });
-    }
-
-    // send to next endpoint
-    next();
   },
   signToken: function ({ username, email, _id }) {
     const payload = { username, email, _id };
